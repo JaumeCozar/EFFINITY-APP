bb52121cc9f4ee6b9a1752dfa90f6b96b7aaaa76
Jaume Cozar Rins <cozar769@gmail.com>
2025-07-08 09:34:52 +0200

    edit empresa redireccionado

--- src/components/UserProfile/UserAddressCard.tsx
+++ src/components/UserProfile/UserAddressCard.tsx
@@ -1,16 +1,7 @@
-import { useModal } from "../../hooks/useModal";
-import { Modal } from "../ui/modal";
-import Button from "../ui/button/Button";
-import Input from "../form/input/InputField";
-import Label from "../form/Label";
+import { useNavigate } from "react-router-dom";
 
 export default function UserAddressCard() {
-  const { isOpen, openModal, closeModal } = useModal();
-  const handleSave = () => {
-    // Handle save logic here
-    console.log("Saving changes...");
-    closeModal();
-  };
+  const navigate = useNavigate();
   return (
     <>
       <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
@@ -32,7 +23,7 @@ export default function UserAddressCard() {
 
               <div>
                 <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                  Ciudad
+                  Dirección
                 </p>
                 <p className="text-sm font-medium text-gray-800 dark:text-white/90">
                   [ubi]
@@ -56,11 +47,31 @@ export default function UserAddressCard() {
                   [Hostelería]
                 </p>
               </div>
+
+              {/* Apartado Teléfono */}
+              <div>
+                <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
+                  Teléfono
+                </p>
+                <p className="text-sm font-medium text-gray-800 dark:text-white/90">
+                  [600 123 456]
+                </p>
+              </div>
+
+              {/* Apartado Email */}
+              <div>
+                <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
+                  Email
+                </p>
+                <p className="text-sm font-medium text-gray-800 dark:text-white/90">
+                  [test@email.com]
+                </p>
+              </div>
             </div>
           </div>
 
-          <button
-            onClick={openModal}
+          <button 
+            onClick={() => navigate('/registro-empresa')}
             className="flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200 lg:inline-flex lg:w-auto"
           >
             <svg
@@ -82,51 +93,6 @@ export default function UserAddressCard() {
           </button>
         </div>
       </div>
-      <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[700px] m-4">
-        <div className="relative w-full p-4 overflow-y-auto bg-white no-scrollbar rounded-3xl dark:bg-gray-900 lg:p-11">
-          <div className="px-2 pr-14">
-            <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
-              Editar dirección
-            </h4>
-            <p className="mb-6 text-sm text-gray-500 dark:text-gray-400 lg:mb-7">
-              Actualiza tus datos para mantener tu perfil al día.
-            </p>
-          </div>
-          <form className="flex flex-col">
-            <div className="px-2 overflow-y-auto custom-scrollbar">
-              <div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
-                <div>
-                  <Label>País</Label>
-                  <Input type="text" value="España" />
-                </div>
-
-                <div>
-                  <Label>Ciudad/Provincia</Label>
-                  <Input type="text" value="Tarragona, España" />
-                </div>
-
-                <div>
-                  <Label>Código Postal</Label>
-                  <Input type="text" value="43101" />
-                </div>
-
-                <div>
-                  <Label>Sector</Label>
-                  <Input type="text" value="Hostelería" />
-                </div>
-              </div>
-            </div>
-            <div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
-              <Button size="sm" variant="outline" onClick={closeModal}>
-                Cerrar
-              </Button>
-              <Button size="sm" onClick={handleSave}>
-                Guardar cambios
-              </Button>
-            </div>
-          </form>
-        </div>
-      </Modal>
     </>
   );
 }
