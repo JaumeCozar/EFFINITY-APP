b8733ab2b6a1b0b200edbfbda8122e50a5e1a618
Jordi Salvat <jordisalvat03@gmail.com>
2025-07-15 08:53:12 +0200

    puesto el / kg al lado del precio de alimentos

--- src/components/tables/BasicTables/BasicTableOneModCocina.tsx
+++ src/components/tables/BasicTables/BasicTableOneModCocina.tsx
@@ -198,7 +198,7 @@ export default function BasicTableOneModCocina() {
                   </TableCell>
 
                   <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
-                    {user.precio}€
+                    {user.precio}€ / kg
                   </TableCell>
 
                   <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
