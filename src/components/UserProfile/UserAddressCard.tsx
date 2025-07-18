1b607012a3007b029bcc619001943ff15d0abca7
Jeynner <jeynnerg@gmail.com>
2025-07-18 10:11:30 +0200

    Modificado el perfil de usuario, el boton editar de informacion de la empresa ahora funciona con un modal y al guardar cambios se activa la tostada

--- src/components/UserProfile/UserAddressCard.tsx
+++ src/components/UserProfile/UserAddressCard.tsx
@@ -1,9 +1,81 @@
-import { useNavigate } from "react-router-dom";
 
+import { Modal } from "../ui/modal";
+import { useModal } from "../../hooks/useModal";
+import Button from "../ui/button/Button";
+import Input from "../form/input/InputField";
+import Label from "../form/Label";
+import Select from "../../components/form/Select";
+import { EnvelopeIcon } from "../../icons";
+import PhoneInput from "../../components/form/group-input/PhoneInput";
+import ComponentCard from "../common/ComponentCard";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import { useState, useEffect } from "react";
 export default function UserAddressCard() {
-  const navigate = useNavigate();
+const { isOpen, openModal, closeModal } = useModal();
+
+ const options = [
+    { value: "hotel", label: "Hotel" },
+    { value: "restaurante", label: "Restaurante" },
+    { value: "bar", label: "Bar" },
+  ];
+
+  const optionsPais = [
+    { value: "usa", label: "Estados Unidos" },
+    { value: "es", label: "España" },
+    { value: "fr", label: "Francia" },
+    { value: "pt", label: "Portugal" },
+  ];
+
+  const handleSelectChange = (value: string) => {
+    console.log("Selected value:", value);
+  };
+
+
+  const countries = [
+    { code: "US", label: "+1" },
+    { code: "ES", label: "+34" },
+    { code: "FR", label: "+33" },
+    { code: "PT", label: "+351" },
+  ];
+  const handlePhoneNumberChange = (phoneNumber: string) => {
+    console.log("Updated phone number:", phoneNumber);
+  };
+
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  
+    // Efecto para actualizar el estado cuando cambia localStorage (internamente)
+    useEffect(() => {
+      const observer = setInterval(() => {
+        const current = localStorage.getItem('theme') || 'light';
+        setTheme(prev => (prev !== current ? current : prev));
+      }, 300); // actualiza cada 300ms
+  
+      return () => clearInterval(observer);
+    }, []);
+
+    const saveClickToast = () => {
+    toast.info("Se ha guardado la configuracion");
+    closeModal();
+  };
+
   return (
+    
     <>
+
+    <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={theme}
+        transition={Bounce}
+      />
+
       <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
         <div className="flex flex-col gap-6 lg:flex-row lg:items-start lg:justify-between">
           <div>
@@ -71,7 +143,7 @@ export default function UserAddressCard() {
           </div>
 
           <button 
-            onClick={() => navigate('/registro-empresa')}
+            onClick={openModal}
             className="flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200 lg:inline-flex lg:w-auto"
           >
             <svg
@@ -93,6 +165,76 @@ export default function UserAddressCard() {
           </button>
         </div>
       </div>
+      <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[700px] m-4">
+
+      <div className="relative w-full max-h-[90vh] p-4 overflow-y-auto bg-white no-scrollbar rounded-3xl dark:bg-gray-900 lg:p-11">
+          <ComponentCard title="Informacion de la Empresa">
+
+        <div className="space-y-6">
+        <div>
+          <Label htmlFor="inputOne">Empresa</Label>
+          <Input type="text" id="inputOne" placeholder="Paco de Lucia" />
+        </div>
+
+        <div>
+          <Label htmlFor="inputTwo">Direccion</Label>
+          <Input type="text" id="inputTwo" placeholder="Calle Tarragona 555" />
+        </div>
+
+        <div>
+          <Label htmlFor="inputThree">Codigo Postal</Label>
+          <Input type="text" id="inputThree" placeholder="43101" />
+        </div>
+
+        <div>
+          <Label>Pais</Label>
+          <Select
+            options={optionsPais}
+            placeholder="Selecciona una opcion"
+            onChange={handleSelectChange}
+            className="dark:bg-dark-900"
+          />
+        </div>
+
+        <div>
+          <Label>Telefono</Label>
+          <PhoneInput
+            selectPosition="end"
+            countries={countries}
+            placeholder="+1 (555) 000-0000"
+            onChange={handlePhoneNumberChange}
+          />
+        </div>
+
+        <div>
+          <Label>Email</Label>
+          <div className="relative">
+            <Input
+              placeholder="info@gmail.com"
+              type="text"
+              className="pl-[62px]"
+            />
+            <span className="absolute left-0 top-1/2 -translate-y-1/2 border-r border-gray-200 px-3.5 py-3 text-gray-500 dark:border-gray-800 dark:text-gray-400">
+              <EnvelopeIcon className="size-6" />
+            </span>
+          </div>
+        </div>
+
+        <div>
+          <Label>Sector</Label>
+          <Select
+            options={options}
+            placeholder="Selecciona una opcion"
+            onChange={handleSelectChange}
+            className="dark:bg-dark-900"
+          />
+        </div>
+
+        <Button size="md" onClick={saveClickToast}>Enviar</Button>
+      </div>
+      </ComponentCard>
+      </div>
+      </Modal>
     </>
   );
 }
