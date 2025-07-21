396120f2ce0a85ff029d00de7556c8b132311fc3
Jeynner <jeynnerg@gmail.com>
2025-07-21 12:00:28 +0200

    Arreglados botones cocina, adaptados al responsive

--- src/components/tables/BasicTables/BasicTableOneModCocina.tsx
+++ src/components/tables/BasicTables/BasicTableOneModCocina.tsx
@@ -262,7 +262,7 @@ export default function BasicTableOneModCocina() {
                           fill=""
                         />
                       </svg>
-                      Editar
+                      
                     </button>
                     <button
                       className="flex-1 flex items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
@@ -301,7 +301,7 @@ export default function BasicTableOneModCocina() {
                           fill=""
                         />
                       </svg>
-                      Eliminar
+                      
                     </button>
                   </div>
                 </div>
