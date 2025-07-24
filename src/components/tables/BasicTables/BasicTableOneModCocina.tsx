5459b957a4ac5bd100439081902d41edfd2bf05e
Jordi Salvat <jordisalvat03@gmail.com>
2025-07-25 01:00:04 +0200

    elimine unos divs que sobraban

--- src/components/tables/BasicTables/BasicTableOneModCocina.tsx
+++ src/components/tables/BasicTables/BasicTableOneModCocina.tsx
@@ -205,14 +205,14 @@ export default function BasicTableOneModCocina() {
 
     {/* Botones de acción arriba */}
     <div className="flex justify-end gap-2 w-full">
-      <div className="flex flex-1 items-center justify-center rounded-full px-4 py-2 text-sm font-medium border border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.05]">
+      
         <button
           type="button"
           onClick={(e) => {
             e.stopPropagation();
             console.log("Editar tipo:", tipo.nombre);
           }}
-          className="text-gray-950 dark:text-gray-400"
+          className="flex flex-1 items-center justify-center rounded-full px-4 py-2 text-sm font-medium border border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.05] text-gray-950 dark:text-gray-400"
           title="Editar"
         >
           <svg
@@ -231,14 +231,14 @@ export default function BasicTableOneModCocina() {
                         />
                       </svg>
         </button>
-      </div>
-      <div className="rounded-full px-4 py-2 text-sm font-medium border border-red-300 bg-white hover:bg-red-50 text-red-600 dark:border-red-700 dark:bg-gray-800 dark:hover:bg-white/[0.05]">
+      
         <button
           type="button"
           onClick={(e) => {
             e.stopPropagation();
             console.log("Eliminar tipo:", tipo.nombre);
           }}
+          className="rounded-full px-4 py-2 text-sm font-medium border border-red-300 bg-white hover:bg-red-50 text-red-600 dark:border-red-700 dark:bg-gray-800 dark:hover:bg-white/[0.05]"
           title="Eliminar"
         >
           <svg
@@ -275,7 +275,6 @@ export default function BasicTableOneModCocina() {
                         />
                       </svg>
         </button>
-      </div>
     </div>
 
     {/* Nombre en medio */}
@@ -378,13 +377,13 @@ export default function BasicTableOneModCocina() {
 
     </div >
     <div className="flex  gap-2 mt-auto w-full">
-    <div className="flex flex-1 items-center justify-center rounded-full px-4 py-2 text-sm font-medium border border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.05]">
+    
       <button
         onClick={() => {
           setSelectedUser(user);
           openModal();
         }}
-        className=""
+        className="flex flex-1 items-center justify-center rounded-full px-4 py-2 text-sm font-medium border border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.05]"
       >
         <svg
                         className="fill-current"
@@ -402,12 +401,10 @@ export default function BasicTableOneModCocina() {
                         />
                       </svg>
       </button>
-        </div>
-        <div className="flex flex-1 justify-center rounded-full px-4 py-2 text-sm font-medium border border-red-300 bg-white hover:bg-red-50 text-red-600 dark:border-red-700 dark:bg-gray-800 dark:hover:bg-white/[0.05]">
 
       <button
         onClick={handleDelete}
-        className=""
+        className="flex flex-1 justify-center rounded-full px-4 py-2 text-sm font-medium border border-red-300 bg-white hover:bg-red-50 text-red-600 dark:border-red-700 dark:bg-gray-800 dark:hover:bg-white/[0.05]"
       >
         <svg
                         className="fill-current"
@@ -443,7 +440,6 @@ export default function BasicTableOneModCocina() {
                         />
                       </svg>
       </button>
-      </div>
       
     </div>
   </div>
