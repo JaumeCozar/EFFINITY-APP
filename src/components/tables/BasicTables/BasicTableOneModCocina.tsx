96f21738325fe8b0bd8548311034a0d8966b655f
Jeynner <jeynnerg@gmail.com>
2025-07-24 10:06:34 +0200

    Botones editar y eliminar de alimentos centrados

--- src/components/tables/BasicTables/BasicTableOneModCocina.tsx
+++ src/components/tables/BasicTables/BasicTableOneModCocina.tsx
@@ -240,14 +240,15 @@ export default function BasicTableOneModCocina() {
         {user.tipoAlimento?.nombre} {user.tipoAlimento?.icon}
       </div>
 
-    </div>
-    <div className="flex gap-2 mt-auto w-full">
+    </div >
+    <div className="flex  gap-2 mt-auto w-full">
+    <div className="flex flex-1 items-center justify-center rounded-full px-4 py-2 text-sm font-medium border border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.05]">
       <button
         onClick={() => {
           setSelectedUser(user);
           openModal();
         }}
-        className="flex-1 rounded-full px-4 py-2 text-sm font-medium border border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.05]"
+        className=""
       >
         <svg
                         className="fill-current"
@@ -265,9 +266,12 @@ export default function BasicTableOneModCocina() {
                         />
                       </svg>
       </button>
+        </div>
+        <div className="flex flex-1 justify-center rounded-full px-4 py-2 text-sm font-medium border border-red-300 bg-white hover:bg-red-50 text-red-600 dark:border-red-700 dark:bg-gray-800 dark:hover:bg-white/[0.05]">
+
       <button
         onClick={handleDelete}
-        className="flex-1 rounded-full px-4 py-2 text-sm font-medium border border-red-300 bg-white hover:bg-red-50 text-red-600 dark:border-red-700 dark:bg-gray-800 dark:hover:bg-white/[0.05]"
+        className=""
       >
         <svg
                         className="fill-current"
@@ -303,6 +307,8 @@ export default function BasicTableOneModCocina() {
                         />
                       </svg>
       </button>
+      </div>
+      
     </div>
   </div>
 ))}
