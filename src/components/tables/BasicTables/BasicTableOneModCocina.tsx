067c09a8b65a29ceea15646ac67831a52b3fc31e
Jordi Salvat <jordisalvat03@gmail.com>
2025-07-25 00:46:30 +0200

    radio buttons filtado de tipos de alimentos con sus botones de editar y eliminar

--- src/components/tables/BasicTables/BasicTableOneModCocina.tsx
+++ src/components/tables/BasicTables/BasicTableOneModCocina.tsx
@@ -17,6 +17,10 @@ import { useTheme } from "../../../context/ThemeContext";
 
 
 export default function BasicTableOneModCocina() {
+
+  const [selectedTipo, setSelectedTipo] = useState<number | null>(null);
+ 
+
   const swalWithBootstrapButtons = Swal.mixin({
     customClass: {
       confirmButton:
@@ -150,22 +154,146 @@ export default function BasicTableOneModCocina() {
         description="Esta es la página de Panel de Calendario React.js para TailAdmin - Plantilla de Panel de Administración React.js Tailwind CSS"
       />
       <div className="overflow-x-hidden p-0">
+        <div className="flex items-center justify-between mb-4">
+  <h1 className="text-2xl font-semibold text-gray-800 dark:text-white/90">
+    Tipos de Alimentos 
+    <div className="text-sm sm:text-base font-bold leading-tight break-words max-w-full peer-checked:underline dark:text-gray-500">
+      FILTRAR
+    </div>
+  </h1>
+
+  <div className="flex gap-2">
+    <Button size="sm" onClick={openModal2}>
+      Añadir Tipo de Alimento
+    </Button>
+    <Button size="sm" onClick={openModal2}>
+      Añadir Alimento
+    </Button>
+  </div>
+</div>
+
         <div className="w-full xl:max-w-screen-xl mx-auto overflow-x-auto box-border ">
-          <div className="flex overflow-x-auto scroll-smooth snap-x snap-mandatory space-x-2 scrollbar-hide touch-pan-x pb-3 custom-scrollbar">
-            {tipoAlimentoData.map((item) => (
-              <button
-                key={item.id}
-                id={item.id}
-                className="snap-center snap-always px-4 py-2 rounded whitespace-nowrap text-sm bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300"
-                onClick={() => {
-                  console.log("Has clickeado:", item.nombre);
-                }}
-              >
-                {item.nombre}
-              </button>
-            ))}
+          <div className="flex overflow-x-auto scroll-smooth snap-x snap-mandatory space-x-2 scrollbar-hide touch-pan-x pb-3 pt-2 px-2 scroll-px-4 custom-scrollbar">
+
+            {tipoAlimentoData.map((tipo) => (
+  <label
+    key={tipo.id}
+    className={`relative
+  ${selectedTipo === tipo.id ? "outline-2 outline-green-500 border-green-500 bg-green-100 text-black" : " bg-gray-100 dark:bg-gray-400 border border-gray-300 dark:border-black text-black"}
+  rounded-xl
+  px-4 py-4
+  text-sm font-medium
+  shadow-theme-xs
+  cursor-pointer
+  w-100 h-38
+  flex flex-col justify-between items-center text-center
+  hover:outline-2 hover:outline-green-400
+  hover:bg-green-100 dark:hover:bg-green-100
+  hover:border-green-400 hover:shadow hover:text-black`}
+  >
+    {/* Input checkbox invisible */}
+    <input
+  type="checkbox"
+  className="absolute opacity-0 w-0 h-0"
+  checked={selectedTipo === tipo.id}
+  onChange={() =>
+    setSelectedTipo((prev) => (prev === tipo.id ? null : tipo.id))
+  }
+/>
+
+
+
+    {/* Botones de acción arriba */}
+    <div className="flex justify-end gap-2 w-full">
+      <div className="flex flex-1 items-center justify-center rounded-full px-4 py-2 text-sm font-medium border border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.05]">
+        <button
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            console.log("Editar tipo:", tipo.nombre);
+          }}
+          className="text-gray-950 dark:text-gray-400"
+          title="Editar"
+        >
+          <svg
+                        className="fill-current"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M15.0911 2.78206C14.2125 1.90338 12.7878 1.90338 11.9092 2.78206L4.57524 10.116C4.26682 10.4244 4.0547 10.8158 3.96468 11.2426L3.31231 14.3352C3.25997 14.5833 3.33653 14.841 3.51583 15.0203C3.69512 15.1996 3.95286 15.2761 4.20096 15.2238L7.29355 14.5714C7.72031 14.4814 8.11172 14.2693 8.42013 13.9609L15.7541 6.62695C16.6327 5.74827 16.6327 4.32365 15.7541 3.44497L15.0911 2.78206ZM12.9698 3.84272C13.2627 3.54982 13.7376 3.54982 14.0305 3.84272L14.6934 4.50563C14.9863 4.79852 14.9863 5.2734 14.6934 5.56629L14.044 6.21573L12.3204 4.49215L12.9698 3.84272ZM11.2597 5.55281L5.6359 11.1766C5.53309 11.2794 5.46238 11.4099 5.43238 11.5522L5.01758 13.5185L6.98394 13.1037C7.1262 13.0737 7.25666 13.003 7.35947 12.9002L12.9833 7.27639L11.2597 5.55281Z"
+                          fill=""
+                        />
+                      </svg>
+        </button>
+      </div>
+      <div className="rounded-full px-4 py-2 text-sm font-medium border border-red-300 bg-white hover:bg-red-50 text-red-600 dark:border-red-700 dark:bg-gray-800 dark:hover:bg-white/[0.05]">
+        <button
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            console.log("Eliminar tipo:", tipo.nombre);
+          }}
+          title="Eliminar"
+        >
+          <svg
+                        className="fill-current"
+                        width="18"
+                        height="18"
+                        viewBox="-48 0 512 512"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="m208 416c8.835938 0 16-7.164062 16-16v-128c0-8.835938-7.164062-16-16-16s-16 7.164062-16 16v128c0 8.835938 7.164062 16 16 16zm0 0"
+                          fill=""
+                        />
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="m272 416c8.835938 0 16-7.164062 16-16v-128c0-8.835938-7.164062-16-16-16s-16 7.164062-16 16v128c0 8.835938 7.164062 16 16 16zm0 0"
+                          fill=""
+                        />
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="m144 416c8.835938 0 16-7.164062 16-16v-128c0-8.835938-7.164062-16-16-16s-16 7.164062-16 16v128c0 8.835938 7.164062 16 16 16zm0 0"
+                          fill=""
+                        />
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="m368 64h-96v-48c0-8.835938-7.164062-16-16-16h-96c-8.835938 0-16 7.164062-16 16v48h-96c-26.5.027344-47.9726562 21.5-48 48v32c.0351562 20.316406 12.847656 38.417969 32 45.199219v274.800781c.027344 26.5 21.5 47.972656 48 48h256c26.5-.027344 47.972656-21.5 48-48v-274.800781c19.152344-6.78125 31.964844-24.882813 32-45.199219v-32c-.027344-26.5-21.5-47.972656-48-48zm-192-32h64v32h-64zm176 432c0 8.835938-7.164062 16-16 16h-256c-8.835938 0-16-7.164062-16-16v-272h288zm32-320c0 8.835938-7.164062 16-16 16h-320c-8.835938 0-16-7.164062-16-16v-32c0-8.835938 7.164062-16 16-16h320c8.835938 0 16 7.164062 16 16zm0 0"
+                          fill=""
+                        />
+                      </svg>
+        </button>
+      </div>
+    </div>
+
+    {/* Nombre en medio */}
+    <div className="text-sm sm:text-base font-bold leading-tight break-words max-w-full peer-checked:underline">
+      {tipo.nombre}
+    </div>
+
+    {/* Emoji abajo */}
+    <div className="text-2xl">{tipo.icon}</div>
+  </label>
+))}
+
+
+
+
           </div>
         </div>
+
 
         <div className="w-full">
           <div className="flex gap-5 my-4 max-w-full justify-around">
@@ -207,7 +335,7 @@ export default function BasicTableOneModCocina() {
                 styles={theme === "dark" ? customSelectStylesDark : undefined}
               />
             </div>
-            <div className="flex-1">
+            {/* <div className="flex-1">
               <Button
                 className="w-full"
                 size="sm"
@@ -217,9 +345,17 @@ export default function BasicTableOneModCocina() {
               >
                 Añadir Alimento
               </Button>
-            </div>
+            </div> */}
           </div>
         </div>
+
+
+        <div className="flex items-center justify-center mb-4">
+          <h1 className="text-2xl font-semibold text-gray-800 dark:text-white/90">
+            Alimentos 
+          </h1>
+        </div>
+
 
         <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03] w-full xl:max-w-screen-xl mx-auto box-border">
           <div className="w-full box-border">
